90f2ac31fda1f0826dac1970089f0d71d4c79a0a
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-02-01 11:17:20 +0100

    Updates

---
+++ precache-manifest.86763437d51463e25f696163acb7e5e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c42f27f4cb90db9c68c",
+    "url": "/markdown-previewer/static/css/main.ebf4796a.chunk.css"
+  },
+  {
+    "revision": "3c42f27f4cb90db9c68c",
+    "url": "/markdown-previewer/static/js/main.3c42f27f.chunk.js"
+  },
+  {
+    "revision": "b5aac9b44dbc1cea2200",
+    "url": "/markdown-previewer/static/css/1.3f1eb3e6.chunk.css"
+  },
+  {
+    "revision": "b5aac9b44dbc1cea2200",
+    "url": "/markdown-previewer/static/js/1.b5aac9b4.chunk.js"
+  },
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "50578f8af3d90310472c262857a8caab",
+    "url": "/markdown-previewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/markdown-previewer/precache-manifest.77be24680904ce1878358c2748bad339.js"
+  "/markdown-previewer/precache-manifest.86763437d51463e25f696163acb7e5e6.js"
 );
 
 workbox.clientsClaim();
